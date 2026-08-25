ff34a2898e1941cea3fae69fc4fe37bc507acf52
TheLeBerton <theleberton@gmail.com>
2026-08-25 22:19:23 +0200

    FIX naming for _invulnerability_frames and typing of INVULNERABILITY_FRAMES

--- src/mario_isaac/ecs/component/health.py
+++ src/mario_isaac/ecs/component/health.py
@@ -2,12 +2,12 @@ from .base import Component
 
 
 class HealthComponent(Component):
-    INVULNERABILITY_FRAMES = 60
+    INVULNERABILITY_FRAMES: int = 60
 
     def __init__(self, max_hp: int) -> None:
         self._max_hp: int = max_hp
         self._current_hp: int = max_hp
-        self._invulnverability_frames: int = 0
+        self._invulnerability_frames: int = 0
 
     @property
     def hp(self) -> int:
@@ -22,14 +22,14 @@ class HealthComponent(Component):
         return self._current_hp > 0
 
     def damage(self, amount: int) -> bool:
-        if self._invulnverability_frames > 0:
+        if self._invulnerability_frames > 0:
             return False
 
         self._current_hp = max(0, self._current_hp - amount)
-        self._invulnverability_frames = self.INVULNERABILITY_FRAMES
+        self._invulnerability_frames = self.INVULNERABILITY_FRAMES
         return True
 
     def tick_invulnerability(self) -> None:
-        if self._invulnverability_frames > 0:
-            self._invulnverability_frames -= 1
+        if self._invulnerability_frames > 0:
+            self._invulnerability_frames -= 1
 
