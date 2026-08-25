daa0a87b588272902affa7489386fb97d1fdb753
TheLeBerton <theleberton@gmail.com>
2026-08-25 02:11:02 +0200

    REDESIGN EntityManager storage: type-indexed lookup instead of linear scan

--- src/mario_isaac/ecs/entity_manager.py
+++ src/mario_isaac/ecs/entity_manager.py
@@ -1,10 +1,13 @@
+from typing import cast
+
 from .component import Component
 
 
 class EntityManager:
     def __init__(self) -> None:
         self._last: int = 0
-        self._components: dict[int, list[Component]] = {}
+        self._entities: set[int] = set()
+        self._components: dict[type[Component], dict[int, Component]] = {}
 
     @property
     def last(self) -> int:
@@ -12,38 +15,34 @@ class EntityManager:
 
     def create_entity(self) -> int:
         self._last += 1
-        self._components[self._last] = []
+        self._entities.add(self._last)
         return self._last
 
     def add_component(self, entity: int, component: Component) -> None:
-        if entity > self._last:
+        if entity not in self._entities:
             return 
+        self._components.setdefault(type(component), {})[entity] = component
 
-        components = self._components[entity]
-        if component not in components:
-            self._components[entity].append(component)
 
-    def get_component(self, entity: int, comp_type: type[Component]) -> Component:
-        for comp in self._components[entity]:
-            if isinstance(comp, comp_type):
-                return comp
-        raise ValueError("Component does not exist in the EntityManager")
+    def get_component[T: Component](self, entity: int, comp_type: type[T]) -> T:
+        try:
+            component = self._components[comp_type][entity]
+        except KeyError:
+            raise ValueError("Component does not exist in the EntityManager")
+        return cast(T, component)
 
-    def get_entities_with_component(self, component: type[Component]) -> dict[int, list[Component]]:
-        entities: dict[int, list[Component]] = {}
-        for key, value in self._components.items():
-            if any(isinstance(val, component) for val in value):
-                entities[key] = value
-        return entities
+    def get_entities_with_component(self, comp_type: type[Component]) -> list[int]:
+        return list(self._components.get(comp_type, {}))
 
-    def get_entities_with_components(self, components: list[type[Component]]) -> dict[int, list[Component]]:
-        entities: dict[int, list[Component]] = {}
-        for key, value in self._components.items():
-            if all(any(isinstance(val, comp_type) for val in value) for comp_type in components):
-                entities[key] = value
-        return entities
+    def get_entities_with_components(self, comp_types: list[type[Component]]) -> list[int]:
+        if not comp_types:
+            return []
+        entity_sets = [set(self._components.get(comp_type, {})) for comp_type in comp_types]
+        return list(set.intersection(*entity_sets))
 
     def remove_entity(self, entity_id: int) -> None:
-        _ = self._components.pop(entity_id, None)
+        self._entities.discard(entity_id)
+        for components in self._components.values():
+            components.pop(entity_id, None)
 
 
