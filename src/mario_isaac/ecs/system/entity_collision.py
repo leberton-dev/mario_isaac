2ab28ed635419b49570ec1a271ff63b97d8a5ba0
TheLeBerton <theleberton@gmail.com>
2026-08-25 15:23:30 +0200

    EXTRACT shared AABB collision resolution, use it in EntityCollisionSystem

--- src/mario_isaac/ecs/system/entity_collision.py
+++ src/mario_isaac/ecs/system/entity_collision.py
@@ -5,6 +5,7 @@ import pygame
 from ..component import CollisionComponent, TransformComponent, VelocityComponent
 from ..entity_manager import EntityManager
 from .base import System
+from .collision_resolution import resolve_aabb_collision
 
 if TYPE_CHECKING:
     from ...room import RoomGrid
@@ -17,42 +18,23 @@ class EntityCollisionSystem(System):
 
     @override
     def update(self) -> None:
-        entity_rects: list[pygame.Rect] = []
-        entity_ids: list[int] = []
-
-        for entity_id in self._room_grid.current_room.enemy_ids:
-
-            entity_ids.append(entity_id)
-
-            transform = self._entity_manager.get_component(entity_id, TransformComponent)
-            collision = self._entity_manager.get_component(entity_id, CollisionComponent)
-
-            rect = pygame.Rect(transform.x, transform.y, collision.width, collision.height)
-            entity_rects.append(rect)
+        entity_ids = list(self._room_grid.current_room.enemy_ids) 
+        entity_rects = [self._entity_rect(entity_id) for entity_id in entity_ids]
 
         for idx, entity_id in enumerate(entity_ids):
-
             transform = self._entity_manager.get_component(entity_id, TransformComponent)
             velocity = self._entity_manager.get_component(entity_id, VelocityComponent)
-            collision = self._entity_manager.get_component(entity_id, CollisionComponent)
 
-            old_x = transform.x - velocity.vel_x
-            old_y = transform.y - velocity.vel_y
-
+            old_pos = (int(transform.x - velocity.vel_x), int(transform.y - velocity.vel_y))
             rect = entity_rects.pop(idx)
-            if rect.collidelist(entity_rects) == -1:
-                entity_rects.insert(idx, rect)
-                continue
 
-            rect_x_reverted = pygame.Rect(old_x, transform.y, collision.width, collision.height)
-            rect_y_reverted = pygame.Rect(transform.x, old_y, collision.width, collision.height)
-
-            if rect_x_reverted.collidelist(entity_rects) == -1:
-                transform.x = old_x
-            elif rect_y_reverted.collidelist(entity_rects) == -1:
-                transform.y = old_y
-            else:
-                transform.x = old_x
-                transform.y = old_y
+            transform.x, transform.y = resolve_aabb_collision(rect, old_pos, entity_rects)
 
             entity_rects.insert(idx, rect)
+
+    def _entity_rect(self, entity_id: int) -> pygame.Rect:
+        transform = self._entity_manager.get_component(entity_id, TransformComponent)
+        collision = self._entity_manager.get_component(entity_id, CollisionComponent)
+        return pygame.Rect(transform.x, transform.y, collision.width, collision.height)
+
+
