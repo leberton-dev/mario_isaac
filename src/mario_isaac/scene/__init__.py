a6ba48e910673fcc482a91b0d4f8d5b5a2784654
TheLeBerton <theleberton@gmail.com>
2026-08-25 23:41:42 +0200

    ADD pause scene

--- src/mario_isaac/scene/__init__.py
+++ src/mario_isaac/scene/__init__.py
@@ -1,4 +1,5 @@
 from .menu_scene import PLAY_PRESSED_EVENT_KEY, MenuScene
-from .play_scene import PlayScene
+from .pause_scene import RESUME_REQUESTED_EVENT_KEY, PauseScene
+from .play_scene import PAUSE_REQUESTED_EVENT_KEY, PlayScene
 from .scene import Scene
 from .scene_manager import SceneManager
